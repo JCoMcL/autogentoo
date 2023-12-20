2925185349a3b87f5cd10e79ebcd1aeb74054f86
JCoMcL <rapcyss@gmail.com>
2023-12-20 19:09:52 +0000

    complete basic disk setup

--- scripts/disk-setup.sh
+++ scripts/disk-setup.sh
@@ -1,7 +1,21 @@
-#!/usr/bin/env sh
+#!/usr/bin/env -S sh -e
 
 sfdisk /dev/sda << EOF
-,1G,U
-,+,L
+label: gpt
+
+start=,size= 1G, type=U, name=boot
+start=,size=  +, type=L, name=root
 EOF
+
+# TODO If using an SSD, should check for firmware upgrades
+
+while ! ls /dev/disk/by-partlabel/boot
+	do sleep 0.1
+done
+
+mkfs.vfat -F 32 /dev/disk/by-partlabel/boot
+mkfs.xfs /dev/disk/by-partlabel/root
+
+mkdir -p /mnt/gentoo
+mount /dev/disk/by-partlabel/root /mnt/gentoo
 
