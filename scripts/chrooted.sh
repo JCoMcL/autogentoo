f6ee38693fcd7a09571e7f3e264fb89d27b51685
JCoMcL <rapcyss@gmail.com>
2024-01-07 20:39:35 +0000

    enable binary packages in installation

--- scripts/chrooted.sh
+++ scripts/chrooted.sh
@@ -3,4 +3,20 @@
 mkdir /efi
 mount /dev/disk/by-partlabel/boot /efi
 
-#emerge-webrsync
+emerge-webrsync
+
+#skip eselect profile because the default is what we want for now
+
+#this is important so that it doesn't take 1000 years to compile on VM
+cat << EOF > /etc/portage/binrepos.conf/gentoobinhost.conf
+[binhost]
+priority = 9999
+sync-uri = https://mirror.bytemark.co.uk/gentoo/releases/amd64/binpackages/17.1/x86-64/
+EOF
+
+cat << EOF >> /etc/portage/make.conf
+# Appending getbinpkg to the list of values within the FEATURES variable
+FEATURES="${FEATURES} getbinpkg"
+# Require signatures
+FEATURES="${FEATURES} binpkg-request-signature"
+EOF
