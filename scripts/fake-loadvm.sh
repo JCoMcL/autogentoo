13a2f2943590c00375bf5e5737d3b75422d40ccf
JCoMcL <rapcyss@gmail.com>
2024-02-08 14:56:09 +0000

    make fake snapshots a little less annoying to use

--- scripts/fake-loadvm.sh
+++ scripts/fake-loadvm.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env sh
-echo "this is a fake checkpoint and it does nothing :)"
+
+snapshot="`basename $0`"
+dir="`dirname $0`"
+
+echo "$snapshot is a fake checkpoint and it does nothing :)"
+
+if [[ "$1" = '-p' ]]; then
+	previous_snapshot="$(ls `dirname $0` | sed -n "/^$snapshot$/{x;p;q}; x")"
+	previous_snapshot_path="`dirname $0`/$previous_snapshot"
+	test -f "$previous_snapshot_path" && "$previous_snapshot_path"
+else
+	echo "run with -p to load a prior snapshot"
+fi
--- scripts/loadvm.sh
+++ scripts/loadvm.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
 snapshot="`basename $0`"
-test -n "$1" && snapshot="`basename $1`"
+while [[ -n "$1" ]]; do
+	case "$1" in
+		-p) true;;
+		-*) echo "unrecognized option: $1" 1>&2 && exit 1;;
+		*) snapshot="`basename $1`";;
+	esac
+	shift
+done
 scripts/qemu-cmd.sh loadvm $snapshot
