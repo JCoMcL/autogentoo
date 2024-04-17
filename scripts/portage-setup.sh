0f10daf2699ed049d1521e6a298b0ad4c04ddff7
JCoMcL <j@jcom.cl>
2024-04-17 17:04:55 +0100

    add a workaround to a failure that occures when re-running portage-setup

--- scripts/portage-setup.sh
+++ scripts/portage-setup.sh
@@ -4,7 +4,7 @@ mkdir -p /mnt/gentoo/etc/portage/repos.conf
 cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
 
 #I'm intending to build a portable system, so this probably doesn't make much sense
-cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+cp --dereference  /etc/resolv.conf /mnt/gentoo/etc/ || true as this sometimes fails when run again #TODO better error handling or maybe just remove this line
 
 
 #there is some failure of logic at play here:
