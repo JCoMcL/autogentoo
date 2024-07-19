bb9f11b0c455fd828d5c8d03e52a218617093c57
jcomcl <j@jcom.cl>
2024-07-19 19:43:48 +0100

    add a user interface and as many safety measures as I could to disk-setup.sh, also discover a maybe unfixable bug

--- scripts/disk-setup.sh
+++ scripts/disk-setup.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env -S sh -e
 
-sfdisk /dev/sda << EOF
+DISK="$1"
+# TODO filter out duplicates
+DISK="${DISK:=$(find /dev/disk/by-id -not -name '*-part*' -not -type d | umenu -d "Select the disk to format")}"
+
+#I don't suppose any of you guys know a way to do ansi escape codes that isn't annoying to read and to write
+echo [1mselected disk is [0m[34m[1m$DISK[0m[1m$(readlink "$DISK">/dev/null && echo , a.k.a. [0m[34m[1m$(readlink -e $DISK))[0m
+echo [1mproceeding with format in:
+seq 8 -1 1 | while read n; do echo -n "$n "; sleep 1; done
+echo [0m
+
+sfdisk "$DISK" << EOF
 label: gpt
 
 start=,size= 120M, type=U, name=boot
@@ -9,6 +19,9 @@ EOF
 
 # TODO If using an SSD, should check for firmware upgrades
 
+# FIXME partlabel is ambiguous if we're running from a system that already has disks with these partlabels
+# realistically, there may not be any good, safe way to refer to a partition we just created in shell script
+exit 1
 while ! ls /dev/disk/by-partlabel/boot
 	do sleep 0.1
 done
