8520574f08e3904f03010446a6df39b580230309
jcomcl <j@jcom.cl>
2024-09-01 15:17:58 +0100

    fix a couple of bugs I couldn't test until now, at all seems to work except for the ssh config which will need to be redone as heredoc isn't really practical in a makefile

--- targets/common.mk
+++ targets/common.mk
@@ -1,13 +1,13 @@
 %/:
 	mkdir -p $@
 
-.ONESHELL:
+# This is broken and will need to be redone using Make's "define". ONESHELL is more destructive than I thought and so a heredoc won't really work
 ssh/config: options.mk | ssh/key
 	#      insert indendation ⮧            ⮦ remove blank lines
 	cat <<EOF | sed -e 's/^_/   /' -e '/^$$/d' > $@
-	Host $$(basename $$(pwd))
+	Host $$(basename $$(realpath .))
 	_Hostname ${SSH_ADDRESS}
-	$$(test -n "${SSH_PORT}" && echo_Port ${SSH_PORT})
+	$$(test -n "${SSH_PORT}" && echo Port ${SSH_PORT})
 	_User ${SSH_USER}
 	_UserKnownHostsFile /dev/null
 	_StrictHostKeyChecking no
@@ -19,9 +19,11 @@ ssh/key: | ssh/
 ssh/key.pub: ssh/key
 
 ssh-wrapper/ssh: | ssh/config ssh-wrapper/
-	echo -e "#!/usr/bin/env sh\n$$(which ssh) -F $$(realpath ssh/config) '$$@' > $@
+	echo -e "#!/usr/bin/env sh\n$$(which ssh) -F $$(realpath ssh/config)" '$$@' > $@
 	chmod +x $@
 
 sshpass-wrapper/ssh: | ssh/config sshpass-wrapper/
-	echo -e "#!/usr/bin/env sh\nsshpass -p ${INITIAL_PASSWD} $$(which ssh) -F $$(realpath ssh/config) '$$@'" > $@
+	echo -e "#!/usr/bin/env sh\nsshpass -p ${INITIAL_PASSWD} $$(which ssh) -F $$(realpath ssh/config)" '$$@' | tee $@
 	chmod +x $@
+
+.PHONY: ssh-key-setup
